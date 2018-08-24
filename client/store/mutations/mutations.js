188e2ab95d1f71206add1dcd98829b69224ca009
redye <yonghong.hu@weimob.com>
2018-08-24 17:17:14 +0800

    vuex

---
+++ client/store/mutations/mutations.js
@@ -0,0 +1,5 @@
+export default {
+  updateCount (state, num) { // mutations 只接收两个参数，第二个参数可以是个对象
+    state.count = num
+  }
+}
---
+++ client/store/store.js
@@ -0,0 +1,87 @@
+import Vuex from 'vuex'
+import defaultState from './state/state'
+import mutations from './mutations/mutations'
+import getters from './getters/getters'
+import actions from './actions/actions'
+
+// const store = new Vuex.Store({
+//   state: {
+//     count: 0
+//   },
+//   mutations: {
+//     updateCount (state, num) {
+//       state.count = num
+//     }
+//   }
+// })
+
+// export default store
+
+const isDev = process.env.NODE_ENV === 'development'
+
+export default () => {
+  const store = new Vuex.Store({
+    strict: isDev,
+    state: defaultState,
+    mutations,
+    getters,
+    actions, // mutations 和 actions 都是用来修改出具的，但是 mutations 只能用来同步修改，异步的操作要放到 actions 里面做
+    plugins: [ // 插件按顺序执行
+      (store) => {
+        console.log(`plugin invoked`)
+      }
+    ]
+    // modules: {
+    //   a: {
+    //     namespaced: true,
+    //     state: {
+    //       text: 1
+    //     },
+    //     mutations: {
+    //       updateText (state, text) {
+    //         console.log(`updateText => ${text}`)
+    //         state.text = text
+    //       }
+    //     },
+    //     getters: {
+    //       textPlus (state, getters, rootState) {
+    //         return state.text + rootState.b.text
+    //       }
+    //     },
+    //     actions: {
+    //       add ({state, commit, rootState}) {
+    //         // commit('updateText', rootState.count)
+    //         commit('updateCount', 4567, { root: true })
+    //       }
+    //     }
+    //   },
+    //   b: {
+    //     state: {
+    //       text: 2
+    //     }
+    //   }
+    // }
+  })
+
+  if (module.hot) { // store 热加载
+    module.hot.accept([
+      './state/state',
+      './mutations/mutations',
+      './actions/actions',
+      './getters/getters'
+    ], () => {
+      const newState = require('./state/state').default
+      const newMutations = require('./mutations/mutations').default
+      const newActions = require('./actions/actions').default
+      const newGetters = require('./getters/getters').default
+      store.hotUpdate({
+        state: newState,
+        mutations: newMutations,
+        actions: newActions,
+        getters: newGetters
+      })
+    })
+  }
+
+  return store
+}
